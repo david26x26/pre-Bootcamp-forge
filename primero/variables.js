c6b50557e8cc8aaace6f4c542f4ccf3f1bd9f84f
david26x26 <dax.26.xvid@gmail.com>
2022-10-24 22:33:07 -0500

    first commit

---
+++ primero/variables.js
@@ -0,0 +1,8 @@
+// la variable alturaPermitida es de tipo string y es para que asi se pueda poner la altura en formato de letras
+var alturaPermitida = ">metro";
+
+// la variavble edadPermitida es de tipo numerica para que se pueda poner de manera mas entendible la edad
+var edadPermitida = 11;
+
+console.log(alturaPermitida);
+console.log(edadPermitida);
